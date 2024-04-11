b3febd7009eb24b88cca14e023fa7a5f63120dd3
GakuruUri <urigakuru@icloud.com>
2024-04-11 09:47:47 +0300

    Updated Index .js again

--- Uri_Gakuru_CRC/index.js
+++ Uri_Gakuru_CRC/index.js
@@ -1,8 +1,8 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-	let response = await fetch("https://qfaavdvx3wvkbj4e5akae43zka0twdhp.lambda-url.us-east-1.on.aws/");
-	let data = await response.json();
-	counter.innerHTML = ' Views: ${data}';
+    let response = await fetch("https://qfaavdvx3wvkbj4e5akae43zka0twdhp.lambda-url.us-east-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = data;
 }
 
 updateCounter();
